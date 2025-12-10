c8d2c13aa4131d406e0cb817c549791117882db3
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-10 09:30:08 -0400

    Update __init__.py

--- pony/utils/__init__.py
+++ pony/utils/__init__.py
@@ -1,11 +1,11 @@
 import _strptime as builtins_strptime
 
-from pony.utils._strptime import *
+from pony.utils import _strptime
 from pony.utils._utils import *
 from pony.utils.utils import *
 from pony.utils.properties import *
 
 # patch the builtin module with our faster _strptime utils
-setattr(builtins_strptime, "_strptime", _strptime)
-setattr(builtins_strptime, "_strptime_time", _strptime_time)
-setattr(builtins_strptime, "_strptime_datetime", _strptime_datetime)
+setattr(builtins_strptime, "_strptime", _strptime._strptime)
+setattr(builtins_strptime, "_strptime_time", _strptime._strptime_time)
+setattr(builtins_strptime, "_strptime_datetime", _strptime._strptime_datetime)
