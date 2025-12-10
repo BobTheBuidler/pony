ddde26b461f55726973a06b689a4e2fcd832ccc1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-10 07:37:49 -0400

    Create _utils.py

---
+++ pony/utils/_utils.py
@@ -0,0 +1,30 @@
+# These were in utils.py but we're not ready to compile the full file yet
+from datetime import datetime, strptime
+
+_CACHE_MAXSIZE: Final = 10_000
+_TIMESTAMP_TO_DATETIME: Final[Dict[str, datetime]] = {}
+
+def current_timestamp():
+    return datetime2timestamp(datetime.now())
+
+def datetime2timestamp(d: datetime) -> str:
+    result = d.isoformat(' ')
+    if len(result) == 19: return result + '.000000'
+    return result
+
+def timestamp2datetime(t: str) -> datetime:
+    # we keep the cache in order of last usage
+    dt = _TIMESTAMP_TO_DATETIME.pop(t, None)
+    if dt is None:
+        time_tuple = strptime(t[:19], '%Y-%m-%d %H:%M:%S')
+        microseconds = int((t[20:26] + '000000')[:6])
+        dt = datetime(*time_tuple[:6], microseconds)
+
+    _TIMESTAMP_TO_DATETIME[t] = dt
+
+    # trim the cache if necessary
+    while len(_TIMESTAMP_TO_DATETIME) >= _CACHE_MAXSIZE:
+        first_key = next(iter(_TIMESTAMP_TO_DATETIME))
+        _TIMESTAMP_TO_DATETIME.pop(first_key)
+
+    return dt
