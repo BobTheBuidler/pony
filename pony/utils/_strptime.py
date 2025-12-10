aa780a01b484c007028d193d6955602074160ed5
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-10 09:18:32 -0400

    Update _strptime.py

--- pony/utils/_strptime.py
+++ pony/utils/_strptime.py
@@ -22,7 +22,7 @@ from datetime import (date as datetime_date,
                       timedelta as datetime_timedelta,
                       timezone as datetime_timezone)
 from _thread import allocate_lock as _thread_allocate_lock
-from typing import Dict, Final, List, Optional, Tuple, Type, TypeVar
+from typing import Dict, Final, Iterable, Optional, Tuple, Type, TypeVar
 
 __all__ = []
 
@@ -222,7 +222,7 @@ class TimeRE(Dict[str, str]):
         base.__setitem__('x', self.pattern(self.locale_time.LC_date))
         base.__setitem__('X', self.pattern(self.locale_time.LC_time))
 
-    def __seqToRE(self, to_convert: List[str], directive: str) -> str:
+    def __seqToRE(self, to_convert: Iterable[str], directive: str) -> str:
         """Convert a list to a regex string for matching a directive.
 
         Want possible matching values to be from longest to shortest.  This
@@ -267,9 +267,9 @@ class TimeRE(Dict[str, str]):
 _cache_lock: Final = _thread_allocate_lock()
 # DO NOT modify _TimeRE_cache or _regex_cache without acquiring the cache lock
 # first!
-_TimeRE_cache: Final = TimeRE()
+_TimeRE_cache = TimeRE()
 _CACHE_MAX_SIZE: Final = 5 # Max number of regexes stored in _regex_cache
-_regex_cache: Final[Dict[str, re.Match]] = {}
+_regex_cache: Final[Dict[str, re.Match[str]]] = {}
 
 def _calc_julian_from_U_or_W(year: int, week_of_year: int, day_of_week: int, week_starts_Mon: bool) -> int:
     """Calculate the Julian day based on the year, week of the year, and day of
@@ -318,7 +318,7 @@ def _strptime(data_string: str, format: str = "%a %b %d %H:%M:%S %Y") -> Tuple[T
             msg = "strptime() argument {} must be str, not {}"
             raise TypeError(msg.format(index, type(arg)))
 
-    global _TimeRE_cache, _regex_cache
+    global _TimeRE_cache
     with _cache_lock:
         locale_time = _TimeRE_cache.locale_time
         if (_getlang() != locale_time.lang or
@@ -346,7 +346,7 @@ def _strptime(data_string: str, format: str = "%a %b %d %H:%M:%S %Y") -> Tuple[T
             except IndexError:
                 raise ValueError("stray %% in format '%s'" % format) from None
             _regex_cache[format] = format_regex
-    found = format_regex.match(data_string)
+    found = format_regex.match(data_string)  # type: ignore =union-attr]
     if not found:
         raise ValueError("time data %r does not match format %r" %
                          (data_string, format))
@@ -571,10 +571,10 @@ def _strptime_datetime(cls: Type[T], data_string: str, format: str = "%a %b %d %
     tt, fraction, gmtoff_fraction = _strptime(data_string, format)
     tzname, gmtoff = tt[-2:]
     if gmtoff is None:
-        return cls(*tt[:6], fraction)
+        return cls(*tt[:6], fraction)  # type: ignore [misc]
     tzdelta = datetime_timedelta(seconds=gmtoff, microseconds=gmtoff_fraction)
     if tzname:
         tz = datetime_timezone(tzdelta, tzname)
     else:
         tz = datetime_timezone(tzdelta)
-    return cls(*tt[:6], fraction, tz)
+    return cls(*tt[:6], fraction, tz)  # type: ignore [misc]
