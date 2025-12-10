8a4aaa7e903de2ff302634c6e0c7264df9f98192
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-10 12:59:49 -0400

    Update _sqlite.py

--- pony/orm/dbproviders/_sqlite.py
+++ pony/orm/dbproviders/_sqlite.py
@@ -18,34 +18,34 @@ class SQLiteValue(Value):
         return Value.__str__(self)
 
 class SQLiteDateConverter(dbapiprovider.DateConverter):
-    def sql2py(converter, val: str) -> date:
+    def sql2py(converter, val: str) -> datetime.date:
         try:
             time_tuple = time.strptime(val[:10], '%Y-%m-%d')
             return datetime.date(*time_tuple[:3])
         except: return val
-    def py2sql(converter, val: date) -> str:
+    def py2sql(converter, val: datetime.date) -> str:
         return val.strftime('%Y-%m-%d')
 
 class SQLiteTimeConverter(dbapiprovider.TimeConverter):
-    def sql2py(converter, val: str) -> time:
+    def sql2py(converter, val: str) -> datetime.time:
         try:
             if len(val) <= 8: dt = datetime.strptime(val, '%H:%M:%S')
             else: dt = datetime.strptime(val, '%H:%M:%S.%f')
             return dt.datetime.time()
         except: return val
-    def py2sql(converter, val: time) -> str:
+    def py2sql(converter, val: datetime.time) -> str:
         return val.isoformat()
 
 class SQLiteTimedeltaConverter(dbapiprovider.TimedeltaConverter):
-    def sql2py(converter, val: float) -> timedelta:
+    def sql2py(converter, val: float) -> datetime.timedelta:
         return datetime.timedelta(days=val)
-    def py2sql(converter, val: timedelta) -> float:
+    def py2sql(converter, val: datetime.timedelta) -> float:
         return val.days + (val.seconds + val.microseconds / 1000000.0) / 86400.0
 
 class SQLiteDatetimeConverter(dbapiprovider.DatetimeConverter):
-    def sql2py(converter, val: str) -> datetime:
+    def sql2py(converter, val: str) -> datetime.datetime:
         try: return timestamp2datetime(val)
         except: return val
-    def py2sql(converter, val: datetime) -> str:
+    def py2sql(converter, val: datetime.datetime) -> str:
         return datetime2timestamp(val)
     
