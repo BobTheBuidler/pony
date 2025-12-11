2fe8fab365c0413728cb0a2ed79f7ce4f05f6c48
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-10 22:46:22 -0400

    Create _ormtypes.py

---
+++ pony/orm/_ormtypes.py
@@ -0,0 +1,140 @@
+import types
+from typing import Dict, Final, Tuple
+
+from typing_extensions import Self
+
+from pony.utils import throw, parse_expr, deref_proxy
+
+
+Parsed = Tuple[Tuple[str, ...], Tuple[types.CodeType, ...]]
+
+raw_sql_cache: Final[Dict[str, Parsed]] = {}
+
+function_types = {type, types.FunctionType, types.BuiltinFunctionType}
+
+def parse_raw_sql(sql: str) -> Parsed:
+    result = raw_sql_cache.get(sql)
+    if result is not None: return result
+    if not isinstance(sql, str) or not sql:
+        throw(TypeError, "Raw SQL string fragment expected. Got: %r" % sql)
+    items = []
+    codes = []
+    pos = 0
+    while True:
+        try: i = sql.index('$', pos)
+        except ValueError:
+            items.append(sql[pos:])
+            break
+        items.append(sql[pos:i])
+        if sql[i+1] == '$':
+            items.append('$')
+            pos = i+2
+        else:
+            try: expr, _ = parse_expr(sql, i+1)
+            except ValueError:
+                raise ValueError(sql[i:])
+            pos = i+1 + len(expr)
+            if expr.endswith(';'): expr = expr[:-1]
+            code = compile(expr, '<?>', 'eval')  # expr correction check
+            codes.append(code)
+            items.append((expr, code))
+    result = tuple(items), tuple(codes)
+    raw_sql_cache[sql] = result
+    return result
+
+
+def raw_sql(sql: str, result_type=None) -> "RawSQL":  # type: ignore [no-untyped-def]
+    globals = sys._getframe(1).f_globals
+    locals = sys._getframe(1).f_locals
+    return RawSQL(sql, globals, locals, result_type)
+
+
+@final
+class RawSQL:
+    def __deepcopy__(self, memo) -> Self:
+        assert False  # should not attempt to deepcopy RawSQL instances, because of locals/globals
+    def __init__(self, sql: str, globals: Dict[str, Any] = None, locals: Dict[str, Any] = None, result_type=None):  # type: ignore[no-untyped-def]
+        self.sql: Final = sql
+        items, codes = parse_raw_sql(sql)
+        self.items: Final = items
+        self.codes: Final = codes
+        types, values = normalize(tuple(eval(code, globals, locals) for code in self.codes))
+        self.types: Final = types
+        self.values: Final = values
+        self.result_type: Final = result_type
+    def _get_type_(self) -> "RawSQLType":
+        return RawSQLType(self.sql, self.items, self.types, self.result_type)
+
+
+@final
+class RawSQLType:
+    def __deepcopy__(self, memo) -> Self:  # type: ignore[no-untyped-def]
+        return self  # RawSQLType instances are "immutable"
+    def __init__(self, sql: str, items: Tuple[str, ...], types: tuple, result_type) -> None:  # type: ignore[no-untyped-def]
+        self.sql: Final = sql
+        self.items: Final = items
+        self.types: Final = types
+        self.result_type: Final = result_type
+    def __hash__(self) -> int:
+        return hash(self.sql) ^ hash(self.types)
+    def __eq__(self, other: Any) -> bool:
+        return type(other) is RawSQLType and self.sql == other.sql and self.types == other.types
+    def __ne__(self, other: Any) -> bool:
+        return not self.__eq__(other)
+
+
+# TODO: add overloads
+def normalize(value: Any) -> Tuple[Any, Any]:
+    value = deref_proxy(value)
+    t = type(value)
+    if t is tuple:
+        item_types, item_values = [], []
+        for item in value:
+            item_type, item_value = normalize(item)
+            item_values.append(item_value)
+            item_types.append(item_type)
+        return tuple(item_types), tuple(item_values)
+
+    if t.__name__ == 'EntityMeta':
+        return SetType(value), value
+
+    if t.__name__ == 'EntityIter':
+        from pony.orm.ormtypes import SetType
+        entity = value.entity
+        return SetType(entity), entity
+
+    if isinstance(value, str):
+        return str, value
+
+    if t in function_types:
+        from pony.orm.ormtypes import FuncType
+        return FuncType(value), value
+
+    if t is types.MethodType:
+        from pony.orm.ormtypes import MethodType
+        return MethodType(value), value
+
+    if hasattr(value, '_get_type_'):
+        return value._get_type_(), value
+
+    return normalize_type(t), value
+
+
+# TODO: add overloads
+#def normalize_type(t: type) -> type:
+def normalize_type(t: Any) -> Any:
+    tt = type(t)
+    if tt is tuple: return tuple(map(normalize_type, t))
+    if not isinstance(t, type):
+        return t
+    assert t.__name__ != 'EntityMeta'
+    if tt.__name__ == 'EntityMeta': return t
+    if t is NoneType: return t
+    from pony.orm.ormtypes import Array, Json, primitive_types, type_normalization_dict
+    t = type_normalization_dict.get(t, t)
+    if t in primitive_types: return t
+    if t in (slice, type(Ellipsis)): return t
+    if issubclass(t, str): return str
+    if issubclass(t, (dict, Json)): return Json
+    if issubclass(t, Array): return t
+    throw(TypeError, 'Unsupported type %r' % t.__name__)
