956c006a878e8703ec3f822d83fc518616372b3a
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-14 05:59:11 -0400

    Update _sqlite.py

--- pony/orm/dbproviders/_sqlite.py
+++ pony/orm/dbproviders/_sqlite.py
@@ -1,9 +1,20 @@
-import datetime, time
-from typing import List, Union, cast
+import re
+import datetime, json, time
+from typing import Any, Callable, Final, TypeVar, cast, overload
+
+from typing_extensions import Concatenate, ParamSpec
 
 from pony.orm import dbapiprovider
 from pony.orm._sqlbuilding import Value
 from pony.utils import datetime2timestamp, timestamp2datetime
+
+
+_T = TypeVar("_T")
+_P = ParamSpec("_P")
+_L = TypeVar("_L", bound=list[Any])
+
+_dumps: Final = json.dumps
+_loads: Final = json.loads
 
 
 class SQLiteValue(Value):
@@ -18,7 +29,7 @@ class SQLiteValue(Value):
         return super().__str__()
 
 class SQLiteDateConverter(dbapiprovider.DateConverter):
-    def sql2py(converter, val: str) -> Union[datetime.date, str]:
+    def sql2py(converter, val: str) -> datetime.date | str:
         try:
             time_tuple = time.strptime(val[:10], '%Y-%m-%d')
             return datetime.date(*time_tuple[:3])
@@ -27,7 +38,7 @@ class SQLiteDateConverter(dbapiprovider.DateConverter):
         return val.strftime('%Y-%m-%d')
 
 class SQLiteTimeConverter(dbapiprovider.TimeConverter):
-    def sql2py(converter, val: str) -> Union[datetime.time, str]:
+    def sql2py(converter, val: str) -> datetime.time | str:
         try:
             dt = datetime.strptime(val, '%H:%M:%S' if len(val) <= 8 else '%H:%M:%S.%f')  # type: ignore [attr-defined]
             return cast(datetime.time, dt.datetime.time())
@@ -42,9 +53,150 @@ class SQLiteTimedeltaConverter(dbapiprovider.TimedeltaConverter):
         return val.days + (val.seconds + val.microseconds / 1000000.0) / 86400.0
 
 class SQLiteDatetimeConverter(dbapiprovider.DatetimeConverter):
-    def sql2py(converter, val: str) -> Union[datetime.datetime, str]:
+    def sql2py(converter, val: str) -> datetime.datetime | str:
         try: return timestamp2datetime(val)
         except: return val
     def py2sql(converter, val: datetime.datetime) -> str:
         return datetime2timestamp(val)
     
+
+def dumps(items):
+    return _dumps(items, **SQLiteJsonConverter.json_kwargs)
+
+def py_json_unwrap(value: Any) -> str | None:
+    # "[null,some_json]" -> "some_json"
+    if isinstance(value, str) and value.startswith('[null,'):
+        return value[6:-1]
+    return None
+
+path_cache: Final[dict[Any, Any]] = {}
+
+json_path_re: Final = re.compile(r'\[(-?\d+)\]|\.(?:(\w+)|"([^"]*)")', re.UNICODE)
+
+def _parse_path(path: Any) -> Any:
+    if path in path_cache:
+        return path_cache[path]
+    keys = None
+    if isinstance(path, str) and path.startswith('$'):
+        keys = []
+        pos = 1
+        path_len = len(path)
+        while pos < path_len:
+            match = json_path_re.match(path, pos)
+            if match is not None:
+                g1, g2, g3 = match.groups()
+                keys.append(int(g1) if g1 else g2 or g3)
+                pos = match.end()
+            else:
+                keys = None
+                break
+        else: keys = tuple(keys)
+    path_cache[path] = keys
+    return keys
+
+def _traverse(obj: Any, keys: Any) -> Any:
+    if keys is None: return None
+    list_or_dict = (list, dict)
+    for key in keys:
+        if type(obj) is list:
+            try: obj = obj[key]
+            except IndexError: return None
+        elif type(obj) is dict:
+            try: obj = obj[key]
+            except KeyError: return None
+        else: return None
+    return obj
+
+def _extract(expr: str, *paths: Any) -> Any:
+    expr = _loads(expr) if isinstance(expr, str) else expr
+    result = []
+    for path in paths:
+        keys = _parse_path(path)
+        result.append(_traverse(expr, keys))
+    return result[0] if len(paths) == 1 else result
+
+def py_json_extract(expr: str, *paths: Any) -> str:
+    result = _extract(expr, *paths)
+    if type(result) in (list, dict):
+        result = _dumps(result, **SQLiteJsonConverter.json_kwargs)
+    return result
+
+def py_json_query(expr: str, path: Any, with_wrapper: bool) -> str:
+    result = _extract(expr, path)
+    if type(result) not in (list, dict):
+        if not with_wrapper: return None
+        result = [result]
+    return _dumps(result, **SQLiteJsonConverter.json_kwargs)
+
+def py_json_value(expr: str, path: Any) -> Any:
+    result = _extract(expr, path)
+    return result if type(result) not in (list, dict) else None
+
+def py_json_contains(expr: str, path: Any, key: Any) -> bool:
+    expr = _loads(expr) if isinstance(expr, str) else expr
+    keys = _parse_path(path)
+    expr = _traverse(expr, keys)
+    return type(expr) in (list, dict) and key in expr
+
+def py_json_nonzero(expr: str, path: Any) -> bool:
+    expr = _loads(expr) if isinstance(expr, str) else expr
+    keys = _parse_path(path)
+    expr = _traverse(expr, keys)
+    return bool(expr)
+
+def py_json_array_length(expr: str, path: Any = None) -> int:
+    expr = _loads(expr) if isinstance(expr, str) else expr
+    if path:
+        keys = _parse_path(path)
+        expr = _traverse(expr, keys)
+    return len(expr) if type(expr) is list else 0
+
+def wrap_array_func(func: Callable[Concatenate[_L, _P], _T]) -> Callable[Concatenate[_L, _P], _T]:
+    @wraps(func)
+    def new_func(array: _L, *args: _P.args) -> _T:
+        if array is None:
+            return None
+        array = _loads(array)
+        return func(array, *args)
+    return new_func
+
+@wrap_array_func
+def py_array_index(array: list[_T], index: int) -> _T | None:
+    try:
+        return array[index]
+    except IndexError:
+        return None
+
+@wrap_array_func
+def py_array_contains(array: list[Any], item: Any) -> bool:
+    return item in array
+
+@wrap_array_func
+def py_array_subset(array: list[Any], items: Any) -> bool | None:
+    if items is None: return None
+    items = _loads(items)
+    return set(items).issubset(set(array))
+
+@wrap_array_func
+def py_array_length(array: list[Any]) -> int:
+    return len(array)
+
+@wrap_array_func
+def py_array_slice(array: list[Any], start: int, stop: int) -> str:
+    return dumps(array[start:stop])
+
+def py_make_array(*items: Any) -> str:
+    return dumps(items)
+
+@overload
+def py_string_slice(s: str, start: int | str, end: int | str) -> str: ...
+@overload
+def py_string_slice(s: None, start: int | str, end: int | str) -> None: ...
+def py_string_slice(s: str | None, start: int | str, end: int | str) -> str | None:
+    if s is None:
+        return None
+    if isinstance(start, str):
+        start = int(start)
+    if isinstance(end, str):
+        end = int(end)
+    return s[start:end]
