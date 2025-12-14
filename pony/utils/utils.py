69ea8cb9687a25982a40fbe0eac69d3c306e1df9
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-14 04:57:59 -0400

    Update utils.py

--- pony/utils/utils.py
+++ pony/utils/utils.py
@@ -16,7 +16,7 @@ import pony
 from pony import options
 
 from pony.thirdparty.decorator import decorator as _decorator
-from pony.utils._utils import camelcase_name, coalesce, codeobjects, current_timestamp, datetime2timestamp, distinct, get_codeobject_id, group_concat, is_ident, is_utf8, lowercase_name, mixedcase_name, parse_expr, split_name, strjoin, timestamp2datetime, tostring, uppercase_name
+from pony.utils._utils import camelcase_name, coalesce, codeobjects, current_timestamp, datetime2timestamp, distinct, get_codeobject_id, get_lambda_args, group_concat, is_ident, is_utf8, lambda_args_cache, lowercase_name, mixedcase_name, parse_expr, split_name, strjoin, timestamp2datetime, tostring, truncate_repr, uppercase_name
 
 if pony.MODE.startswith('GAE-'): localbase = object
 else: from threading import local as localbase
@@ -102,56 +102,6 @@ def throw(exc_type: Union[Type[Exception], Exception], *args: Any, **kwargs: Any
         else:
             raise exc  # Set "pony.options.CUT_TRACEBACK = False" to see full traceback
     finally: del exc
-
-def truncate_repr(s, max_len=100):
-    s = repr(s)
-    return s if len(s) <= max_len else s[:max_len-3] + '...'
-
-lambda_args_cache = {}
-
-def get_lambda_args(func):
-    if type(func) is types.FunctionType:
-        codeobject = func.__code__
-        cache_key = get_codeobject_id(codeobject)
-    elif isinstance(func, ast.Lambda):
-        cache_key = func
-    else: assert False  # pragma: no cover
-
-    names = lambda_args_cache.get(cache_key)
-    if names is not None: return names
-
-    if type(func) is types.FunctionType:
-        if hasattr(inspect, 'signature'):
-            names, argsname, kwname, defaults = [], None, None, []
-            for p in inspect.signature(func).parameters.values():
-                if p.default is not p.empty:
-                    defaults.append(p.default)
-
-                if p.kind == p.POSITIONAL_OR_KEYWORD:
-                    names.append(p.name)
-                elif p.kind == p.VAR_POSITIONAL:
-                    argsname = p.name
-                elif p.kind == p.VAR_KEYWORD:
-                    kwname = p.name
-                elif p.kind == p.POSITIONAL_ONLY:
-                    throw(TypeError, 'Positional-only arguments like %s are not supported' % p.name)
-                elif p.kind == p.KEYWORD_ONLY:
-                    throw(TypeError, 'Keyword-only arguments like %s are not supported' % p.name)
-                else: assert False
-        else:
-            names, argsname, kwname, defaults = inspect.getargspec(func)
-    elif isinstance(func, ast.Lambda):
-        argsname = func.args.vararg
-        kwname = func.args.kwarg
-        defaults = func.args.defaults + func.args.kw_defaults
-        names = [arg.arg for arg in func.args.args]
-    else: assert False  # pragma: no cover
-    if argsname: throw(TypeError, '*%s is not supported' % argsname)
-    if kwname: throw(TypeError, '**%s is not supported' % kwname)
-    if defaults: throw(TypeError, 'Defaults are not supported')
-
-    lambda_args_cache[cache_key] = names
-    return names
 
 def error_method(*args, **kwargs):
     raise TypeError()
