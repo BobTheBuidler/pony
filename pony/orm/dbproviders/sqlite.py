d1965879164adebb7421dccb011f71854ce14efd
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-14 05:59:35 -0400

    Update sqlite.py

--- pony/orm/dbproviders/sqlite.py
+++ pony/orm/dbproviders/sqlite.py
@@ -17,7 +17,10 @@ from pony.orm.ormtypes import Json, TrackedArray
 from pony.orm.sqltranslation import SQLTranslator, StringExprMonad
 from pony.orm.sqlbuilding import SQLBuilder, Value, join, make_unary_func
 from pony.orm.dbapiprovider import DBAPIProvider, Pool, wrap_dbapi_exceptions
-from pony.orm.dbproviders._sqlite import SQLiteValue, SQLiteDateConverter, SQLiteDatetimeConverter, SQLiteTimeConverter, SQLiteTimedeltaConverter, dumps, json_path_re, path_cache, py_array_contains, py_array_index, py_array_length, py_array_slice, py_array_subset, py_json_array_length, py_json_contains, py_json_extract, py_json_nonzero, py_json_query, py_json_unwrap, py_json_value, py_make_array py_string_slice, wrap_array_func, _extract, _parse_path, _traverse
+from pony.orm.dbproviders._sqlite import SQLiteValue, SQLiteDateConverter, SQLiteDatetimeConverter, SQLiteTimeConverter, SQLiteTimedeltaConverter, \
+    dumps, json_path_re, path_cache, py_array_contains, py_array_index, py_array_length, py_array_slice, py_array_subset, py_json_array_length, \
+    py_json_contains, py_json_extract, py_json_nonzero, py_json_query, py_json_unwrap, py_json_value, py_make_array py_string_slice, wrap_array_func, \
+    _extract, _parse_path, _traverse
 from pony.utils import datetime2timestamp, timestamp2datetime, absolutize_path, localbase, throw, reraise, \
     cut_traceback_depth
 
