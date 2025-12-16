fe7693b1c831ece6366cf7feb8bad8d8f7a5a29f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-15 22:32:35 -0400

    Update _sqlbuilding.py

--- pony/orm/_sqlbuilding.py
+++ pony/orm/_sqlbuilding.py
@@ -26,7 +26,7 @@ def adapter_numeric(params: tuple["Param", ...]) -> Callable[[list["Value"]], tu
     return adapter
 
 def adapter_named(params: tuple["Param", ...]) -> Callable[[list["Value"]], dict[str, str]]:
-    def adapter(values: list["Value"]) -> dict[str, str]:
+    def adapter(values: dict[str, "Value"]) -> dict[str, str]:
         return {'p%d' % param.id: param.eval(values) for param in params}
     return adapter
 
